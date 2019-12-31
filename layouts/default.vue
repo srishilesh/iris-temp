20a9ad57a1273cf4a398088483bd077371d00dfb
Saiharsha B <34317373+thewhitewolf17@users.noreply.github.com>
2019-12-31 10:37:00 +0530

    Add routes, Remove junk code

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,39 +46,15 @@
         icon
         @click.stop="fixed = !fixed"
       >
-        <v-icon>mdi-minus</v-icon>
       </v-btn>
-      <p>Iris - Faculty Dashboard</p>
+      <v-row justify="center">Iris - Faculty Dashboard</v-row>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :fixed="fixed"
       app
@@ -98,13 +74,28 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-account-plus',
+          title: 'Register',
+          to: '/register'
+        },
+        {
+          icon: 'mdi-login-variant',
+          title: 'Login',
+          to: '/login'
+        },
+        {
+          icon: 'mdi-teach',
+          title: 'Faculty Details',
+          to: '/faculty'
+        },
+        {
+          icon: 'mdi-account-card-details',
+          title: 'Student Details',
+          to: '/student'
         }
       ],
       miniVariant: false,
