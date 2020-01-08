40914241f61cb313b7d922f14c68a4263fb5a060
srishilesh <srishilesh@gmail.com>
2020-01-08 22:59:34 +0530

    With forgot password

--- functions/index.js
+++ functions/index.js
@@ -58,6 +58,67 @@ app.post("/login", function (req, res) {
     });
 })
 
+app.post('/forgotpass', callName); 
+  
+function callName(req, res) { 
+    var spawn = require("child_process").spawn; 
+    var toaddr = req.body.email;                      // NAME AND ID OF THE INPUT FIELD FOR THE EMAIL ADDRESS IS email
+    if(ValidateEmail(toaddr)==false)
+    {
+      return
+    }
+    var otp = generateOTP(5);
+    var date = new Date().toLocaleDateString();
+    var time = new Date().toLocaleTimeString();
+    insertotp(toaddr,otp,date,time);
+    var process = spawn('python',["./email_send.py",(toaddr),(otp),date,time] ); 
+    process.stdout.on('data', function(data) { 
+        res.send({"message":"success"}); 
+    } ) 
+    console.log("Sending email ....")
+}
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
+
+function generateOTP(n) {
+  var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
+  if ( n > max ) {
+          return generate(max) + generate(n - max);
+  }
+
+  max        = Math.pow(10, n+add);
+  var min    = max/10; // Math.pow(10, n) basically
+  var number = Math.floor( Math.random() * (max - min + 1) ) + min;
+
+  return ("" + number).substring(add); 
+}
+
+function insertotp(email,otp,date,time)
+{
+  conn.connect(
+    function (err) { 
+    if (err) { 
+      console.log("!!! Cannot connect !!! Error:");
+      throw err;
+    }
+    else
+    {
+        console.log("Connection established.");
+        conn.query('INSERT INTO iris_db.email_verification (f_email,acc_date,acc_time,otp) VALUES (?,?,?,?);',[email,date,time,otp],
+        function (err, results, fields) {
+          if (err) throw err;
+        console.log('Inserted ' + results.affectedRows + ' row(s).');
+        });
+    }	
+  });
+}
+
 app.post('/register', function(req, res) {
     
 })
