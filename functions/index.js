1302da658c0e0764f1fbcb1d666c45f33864aecd
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-01-12 14:10:44 +0530

    fix error in login

--- functions/index.js
+++ functions/index.js
@@ -30,32 +30,42 @@ app.post("/login", function (req, res) {
     let pass = req.body.pswrd; //entered password
     let rem_chk = req.body.rem; //checkin remember or not
     let message = "";
-
+    console.log("rem "+rem_chk)
     conn.connect(function (err) {
-        if (rem_chk === '1') { //value of remember checkbox is '1'
-            conn.query("UPDATE iris_db.emp_login SET f_remember='1' WHERE f_uname='" + user + "'", function (error, rows, fields) {
-                console.log("Remember column updated");
-            })
-        }
-
-        conn.query("SELECT * FROM iris_db.emp_login", function (error, rows, fields) {
-            console.log("Connected " + user + " " + pass);
-            let length = rows.length;
-            console.log(length);
-
-            for (i = 0; i < length; i++) {
-                if (rows[i].f_email === user) {
-                    if (rows[i].f_remember === '1' || rows[i].f_pword === pass) {
-                        console.log("herheh");
-                        message = "success";
-                    } else {
-                        message = "failure";
-                    }
-                }
+        conn.query("SELECT COUNT(*) AS cnt FROM iris_db.emp_login WHERE f_email='" + user + "'", function(error, rows, fields) {
+            if(rows[0].cnt == 0) {
+                message = "user name and password mismatch";
+                res.send({
+                    "message": message
+                });
             }
-            res.send({
-                "message": message
-            });
+            else {
+                conn.query("SELECT * FROM iris_db.emp_login", function (error, rows, fields) {
+                    console.log("Connected " + user + " " + pass);
+                    let length = rows.length;
+                    console.log(length);
+        
+                    for (i = 0; i < length; i++) {
+                        if (rows[i].f_email === user) {
+                            if (rows[i].f_remember === '1' || rows[i].f_pword === pass) {
+                                console.log("herheh");
+                                message = "success";
+                                if (rem_chk === '1') { //value of remember checkbox is '1'
+                                    conn.query("UPDATE iris_db.emp_login SET f_remember='1' WHERE f_email='" + user + "'", function (error, rows, fields) {
+                                    console.log("Remember column updated");
+                                    })
+                                }
+                            } else {
+                                message = "failure";
+                            }
+                            break;
+                        }
+                    }
+                    res.send({
+                        "message": message
+                    });
+                })
+            }
         })
     });
 })
