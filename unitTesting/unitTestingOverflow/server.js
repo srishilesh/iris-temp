0f0787c0f8a02b4b0d53d5385ddbb67f3b2933cf
Sanjay Tharagesh <sanjay311999@gmail.com>
2020-03-14 13:11:50 +0530

    Update server.js

--- unitTesting/unitTestingOverflow/server.js
+++ unitTesting/unitTestingOverflow/server.js
@@ -26,7 +26,7 @@ app.post("/login", function (req, res) {
     var pass = req.body.pswrd; //entered password
     var rem_chk = req.body.rem; //checkin remember or not
     var message = "";
-    console.log("rem " + rem_chk)
+    // console.log("rem " + rem_chk)
     conn.connect(function (err) {
         conn.query("SELECT COUNT(*) AS cnt FROM iris_db.emp_login WHERE f_email='" + user + "'", function (error, rows, fields) {
             if (rows[0].cnt == 0) {
@@ -36,18 +36,18 @@ app.post("/login", function (req, res) {
                 });
             } else {
                 conn.query("SELECT * FROM iris_db.emp_login", function (error, rows, fields) {
-                    console.log("Connected " + user + " " + pass);
+                    // console.log("Connected " + user + " " + pass);
                     var length = rows.length;
-                    console.log(length);
+                    // console.log(length);
 
                     for (var i = 0; i < length; i++) {
                         if (rows[i].f_email === user) {
                             if (rows[i].f_remember === '1' || rows[i].f_pword === pass) {
-                                console.log("herheh");
+                                // console.log("herheh");
                                 message = "success";
                                 if (rem_chk === '1') { //value of remember checkbox is '1'
                                     conn.query("UPDATE iris_db.emp_login SET f_remember='1' WHERE f_email='" + user + "'", function (error, rows, fields) {
-                                        console.log("Remember column updated");
+                                        // console.log("Remember column updated");
                                     })
                                 }
                             } else {
@@ -71,7 +71,7 @@ app.post('/download_faculty_report', function (req, res) {
     if (ValidateEmail(email) === true) {
         conn.connect(function (err) {
             conn.query('select * from emp_attendance where f_email=?;', [email], function (error, rows, fields) {
-                console.log('leave data retrieved');
+                // console.log('leave data retrieved');
                 var f = 1
                 res.send({
                     message: "success"
@@ -97,7 +97,7 @@ app.post('/download_student_report', function (req, res) {
     if (ValidateEmail(email) === true) {
         conn.connect(function (err) {
             conn.query('select s_roll, department, section, semester, batch, c_date, c_period, attd_status, course_code from student_attendance where f_email=? and c_date=? and course_code=? and department=? and section=? and semester=? and batch=?;', [email, date, course_code, dept, sec, sem, batch], function (error, rows, fields) {
-                console.log('student report data retrieved');
+                // console.log('student report data retrieved');
                 res.send({
                     "message": "success"
                 });
@@ -155,11 +155,33 @@ app.post('/generate_fa_list', function (req, res) {
     });
 })
 
+app.post('/calculate_free_student_leave', function(req, res) {
+    let email = req.body.email; //faculty email id
+    let course_code = req.body.ccode;
+    let dept = req.body.dept;
+    let sec = req.body.section;
+    let sem = req.body.semester;
+    let batch = req.body.batch;
+
+    const min_working_days = 20;
+    const attendance_threshold = 0.5; //50 percent
+
+    conn.connect(function (err) {
+      conn.query(`select s_roll, ?-count(*) as cnt as percent from student_attendance where f_email=? and course_code=? and department=? and section=? and semester=? and batch=?  and (attd_status='1' or attd_status='2') group by s_roll;`, [min_working_days, email, course_code, dept, sec, sem, batch, attendance_threshold], function (error, rows, fields) {
+        // if(err) throw err
+        // console.log('data retrieved');
+        res.send({
+            message: 'success'
+        });
+      })
+    });
+})
+
 /* END MY CODE */
 
 
 app.get('/', function (req, res) {
-    console.log('/: I received a GET request');
+    // console.log('/: I received a GET request');
     var readFileCallback = function (err, data) {
         if (err) {
             res.status(500).send({
@@ -167,14 +189,14 @@ app.get('/', function (req, res) {
             });
             return console.log(err);
         }
-        console.log(data);
+        // console.log(data);
         res.status(200).send(data);
     };
     fs.readFile(filename, 'utf8', readFileCallback);
 });
 app.post('/', function (req, res) {
-    console.log('/: I received a POST request');
-    console.log(req.body);
+    // console.log('/: I received a POST request');
+    // console.log(req.body);
     //res.end({url:'/'});
 
     var readFileCallback = function (err, data) {
@@ -184,7 +206,7 @@ app.post('/', function (req, res) {
             });
             return console.log(err);
         }
-        console.log(data);
+        // console.log(data);
         data += '\n' + req.body.data;
         fs.writeFile(filename, data, (err) => {
             // throws an error, you could also catch it here
