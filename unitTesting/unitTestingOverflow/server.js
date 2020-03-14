98b476487d8108c573ec2016c58004afb36aed51
sanjay311999 <sanjay311999@gmail.com>
2020-03-14 12:33:31 +0530

    add unit test code for generatefalist

--- unitTesting/unitTestingOverflow/server.js
+++ unitTesting/unitTestingOverflow/server.js
@@ -133,6 +133,28 @@ app.post('/update_marks', function (req, res) {
     }
 })
 
+app.post('/generate_fa_list', function (req, res) {
+    let email = req.body.email; //faculty email id
+    let course_code = req.body.ccode;
+    let dept = req.body.dept;
+    let sec = req.body.section;
+    let sem = req.body.semester;
+    let batch = req.body.batch;
+  
+    const total_working_days = 2;
+    const attendance_threshold = 0.5; //50 percent
+  
+    conn.connect(function (err) {
+      conn.query(`select s_roll,course_code, count(*)/? as percent from student_attendance where f_email=? and course_code=? and department=? and section=? and semester=? and batch=?  and (attd_status='1' or attd_status='2') group by s_roll having percent<=?;`, [total_working_days, email, course_code, dept, sec, sem, batch, attendance_threshold], function (error, rows, fields) {
+        // console.log('FA report data retrieved');
+        // res.send(rows);
+        res.send({
+            message: 'FA report generated'
+        });
+      })
+    });
+})
+
 /* END MY CODE */
 
 
